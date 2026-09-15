a92711ada993e90eca0ef5b3fed6f3218075eff1
ehbrown1-gh <ella.h.brown.06@gmail.com>
2026-09-15 19:17:23 -0400

    Fixed server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use(
         cookie: {
             httpOnly: true,
             sameSite: "lax", 
-            secure: process.env.NODE_ENV === "production" || req?.headers['x-forwarded-proto'] === 'https',
+            secure: process.env.NODE_ENV === "production",
             maxAge: 1000 * 60 * 60 * 24
         }
     })
